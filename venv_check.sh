3475fd59580fefce0c8d639eb62198af06c0ccbc
Santi-I-Guess <aquaeagle10@gmail.com>
2025-11-08 13:02:03 -0700

    add bash script for both uv and pip users

---
+++ venv_check.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+handle_uv() {
+    uv venv .venv
+    source .venv/bin/activate
+}
+
+handle_pip() {
+    python3 -m venv .venv
+    source .venv/bin/activate
+    if [[ $(pip list | grep "pygame") == "" ]]; then
+        pip install -r requirements.txt
+    fi
+}
+
+if [[ ! $VIRTUAL_ENV ]]; then
+    if [[ $(command -v uv) ]]; then
+        handle_uv
+    elif [[ $(command -v pip) ]]; then
+        handle_pip
+    else
+        echo "I don't know other package managers. Quitting..."
+    fi
+fi
+
